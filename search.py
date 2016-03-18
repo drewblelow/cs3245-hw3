703041ea5cf1f67e673d3d0b8d45288077be4d82
Andrew Low <andrewjrlow@gmail.com>
2016-03-18 14:31:50 +0800

    due for testing

--- search.py
+++ search.py
@@ -9,6 +9,7 @@ from collections import Counter
 from os.path import basename
 from os import listdir
 from os.path import isfile, join
+from operator import itemgetter
 
 #use message
 def usage():
@@ -61,12 +62,12 @@ def load_dic():
 		DICTIONARY[word] = index
 	docs_line = dic_file.readline()
 	list_docs = docs_line.split()
+	NUM_DOCS = list_docs.length
 	for entry in list_docs:
 		item = entry.split('_')
 		filename = item[0]
 		numwords = item[1]
 		LIST_DOC[filename] = numwords
-	NUM_DOCS = LIST_DOC.len
 	print("[DONE]")
 
 #read and evaluate all queries from file
@@ -74,7 +75,8 @@ def read_queries(file):
 	out_writer = open("outtxt", 'w')
 	with open(file) as fp:
 		for line in fp:
-			evaluate(line)
+			result = evaluate(line)
+			out_writer.write(result + "\n")
 	close(file)
 
 #evaluate one query
@@ -83,7 +85,6 @@ def evaluate(query):
 	seek_pos = open("postings.txt", 'r')
 	seek_pos.seek(0,0)
 	words = query.split()
-	result = []
 	for word in words:
 		if word not in word_score:	
 			if word in DICTIONARY:
@@ -98,18 +99,60 @@ def evaluate(query):
 				#not encountered, score of 0
 				word_score[word] = 0
 		#else duplicate, skip word
+	result = score_query(word_score)
 	return result
 
-#method scores the documents	
+#method scores the documents by word frequency
 def score_documents(list_postings):
-	doc_frequency = list_postings.len
-	idf = math.log(doc_frequency, 10)
+	score_list = []
+	doc_frequency = list_postings.length * 1.0
+	idf_score = float(math.log(NUM_DOCS/doc_frequency, 10))
 	for post in list_postings:
 		item = post.split("_")
 		doc_id = item[0]
 		term_frequency = item[1]
+		tf_score = (1+ math.log(term_frequency, 10))
+		tf_idf = tf_score*idf_score
+		score_item = [doc_id, tf_idf]
+		score_list.append(score_item)
+	return score_list
+
+#method scores the documents by query
+def score_query(score_sheet):
+	doc_scores = {}
+	for entry in score_sheet:
+		doc_id = entry[0]
+		score = entry[1]
+		if doc_id not in doc_scores:
+			doc_scores[doc_id] = score
+		else:
+			current_score = doc_scores[doc_id]
+			new_score = current_score + score
+			doc_scores[doc_id] = new_score
+	list_total_score = []
+	for item in doc_scores:
+		score = doc_scores[item]
+		entry = [item, score]
+		list_total_score.append(entry)
+	#sort by doc score
+	list_total_score.sort(key=itemgetter(1))
+	output = score_to_string(list_total_score)
+	return output
 		
-	
+#score to string method
+def score_to_string(list):
+	if list.length <= 10:
+		output = str(list[0][0])
+		for item in list in range(1, list.length-1):
+			output = output + " " + str(item[0])
+		return output
+	else:
+		top10 = list[:10]
+		output = str(list[0][0])
+		for item in top10 in range(1,9):
+			output = output + " " + str(item[0])
+		return output
+		
 #lines below run the methods defined above
 load_dic()
 read_queries()
